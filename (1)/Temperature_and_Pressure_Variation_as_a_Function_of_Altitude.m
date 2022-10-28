5b81c59f70b170c247e5a8fb07237fde074fa437
Rezky Putra Barens <92147766+rezkybarensstudent@users.noreply.github.com>
2022-10-29 04:07:59 +0700

    Update Files

--- (1)/Temperature_and_Pressure_Variation_as_a_Function_of_Altitude.m
+++ (1)/Temperature_and_Pressure_Variation_as_a_Function_of_Altitude.m
@@ -1,26 +1,27 @@
+% Pressure Distribution in the Sstandard Atmosphere
 % Temperature and Pressure Variation as a Function of Altitude
 
-% Gravitational Constant
+% The Acceleration of Gravity (m/s^2)
 g = 9.81;
-% Atmospheric Pressure
+% Pressure (Pa)
 P0 = 101330;
-% Individual Gas Constant
+% The Perfect Gas Constant (J/(kg K))
 R = 287.13;
 % Number of Points
 np = 60;
-% Temperature
+% Temperature (In Celsius)
 TC = [15, -56.5, -56.4, -44.5, -2.5];
-% Elevation
+% The Vertical Cartesian Coordinate (Elevation (m))
 z = [0, 11000, 20100, 32200, 47300];
-% Generate linearly spaced vectors
+% Generate Linearly Spaced Vectors
 zz = linspace(z(1), z(end), np);
-% Spline used to smooth noisy data and perform interpolation.
+% Cubic Spline Data Interpolation
 t = spline(z, TC+273.15, zz);
-% Subplot divides the current figure into rectangular panes that are numbered row-wise.
+% Create Axes in Tiled Positions
 subplot(1, 2, 1)
-% Plot function in Matlab is used to create a graphical representation of some data.
+% 2-D Line Plot
 plot(t-273.15, zz/1000, 'k-', TC, z/1000, 'ks')
-% Axis equal sets the aspect ratio so that the data units are the same in every direction.
+% Set Axis Limits and Aspect Ratios
 axis([-65, 20, 0, 50])
 % Temperature Label
 xlabel('Temperature (\circC)')
@@ -28,11 +29,12 @@ xlabel('Temperature (\circC)')
 ylabel('Elevation (km)')
 % Subplot 2
 subplot(1, 2, 2)
-% Create an array of all zeros
+% Create An Array of All Zeros
 P = zeros(np,1);
+% The Acceleration of Gravity/The Perfect Gas Constant
 gR = g/R;
 gg = @(h, z, TC) (1./spline(z, TC+273.15, h));
-for k = 1:np;
+for k = 1:np
     Intg = quadl(gg, 0.1, zz(k), [], [], z, TC);
     P(k) = P0*exp(-gR*Intg);
 end
